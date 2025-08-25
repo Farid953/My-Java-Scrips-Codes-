1911137155b6a3e7350779c53afd19d3b7ea1563
faridrahmani <faridrahmani@gmail.com>
2025-08-25 15:19:12 +0430

    Js-1

---
+++ JS-6/script.js
@@ -0,0 +1,37 @@
+// let userName = "ali";
+// console.log(`welcom to our city ${userName}`);
+
+// let i = 0;
+// while (i <= 10) {
+//   console.log(`the i value is: ${i + 1}`);
+
+//   i++;
+// }
+
+// let i = 0;
+// while (i <= 100) {
+//   if (i % 2 != 0) {
+//     console.log(i);
+//   }
+//   i++;
+// }
+
+// let i = 0;
+// while (i <= 100) {
+//   console.log(i);
+//   i += 2;
+// }
+
+let i = 0;
+let userInput;
+let sum = 0;
+
+while (i <= 4) {
+  userInput = +prompt("enter your numbers: ");
+  sum += userInput;
+  i++;
+}
+console.log(sum);
+
+// type conversion Number(), String(), parseInt(), parseFloat()
+// type casting in python int() str() float(0)
